60f388e056cb2be21076513b0c1e5ab0ccaf2d67
Thomas Kuhnert <spielhoelle@posteo.net>
2022-08-31 22:03:36 +0900

    feat: intermediate step

---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# curl -X PUT http://admin:password@localhost:15984/tommy-bot
+# curl -X PUT http://admin:password@localhost:15984/_users
+
+pip3 install --upgrade https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.0.0-py3-none-any.whl
